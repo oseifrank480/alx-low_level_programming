78420ec83ffe629145626b7c58062a3cbe1f8c70
oseifrank480 <oseifrank480@gmail.com>
2023-07-13 15:26:24 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - printing a line of code with the put function
- *Return: 0(success)
+ * main - printing a line of code with the put function
+ * Return: 0(success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
